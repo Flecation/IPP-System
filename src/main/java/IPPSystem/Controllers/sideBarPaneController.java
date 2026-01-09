dff58cc6379842e6568adb1e7edde96e144f79af
Mr.Ant <enthtooaung1234@gmail.com>
2026-01-09 21:07:10 +0630

    navigationPane

--- src/main/java/IPPSystem/Controllers/sideBarPaneController.java
+++ src/main/java/IPPSystem/Controllers/sideBarPaneController.java
@@ -1,4 +1,82 @@
 package IPPSystem.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+
 public class sideBarPaneController {
+
+    @FXML
+    private HBox dashboardViewBtn;
+
+    @FXML
+    private Label dashboardViewIcon;
+
+    @FXML
+    private HBox directoryViewBox;
+
+    @FXML
+    private StackPane loadPane;
+
+    @FXML
+    private Label nameViewText;
+
+    @FXML
+    private Button newTabBtn;
+
+    @FXML
+    private Button notificationViewBtn;
+
+    @FXML
+    private HBox projectViewBtn;
+
+    @FXML
+    private Label projectViewIcon;
+
+    @FXML
+    private Button reloadBtn;
+
+    @FXML
+    private HBox reportViewBtn;
+
+    @FXML
+    private Label reportViewIcon;
+
+    @FXML
+    private Label roleViewText;
+
+    @FXML
+    private Button searchBarClearBtn;
+
+    @FXML
+    private Button searchBarSearchBtn;
+
+    @FXML
+    private TextField searchBarTextField;
+
+    @FXML
+    private HBox searchViewBox;
+
+    @FXML
+    private HBox settingViewBtn;
+
+    @FXML
+    private Label settingViewIcon;
+
+    @FXML
+    private HBox userViewBtn;
+
+    @FXML
+    private Label userViewIcon;
+
+    @FXML
+    private ImageView userViewImage;
+
+    @FXML
+    private Label userViewLbl;
+
 }
